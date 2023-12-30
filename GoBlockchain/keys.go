d17ba5f48f87a42ab1648dd9b6a7b09d4c5bfd1b
Mitch Hamilton <thecryptomitch@gmail.com>
2023-12-30 09:21:11 +0800

    Add better comments to keys file

--- GoBlockchain/keys.go
+++ GoBlockchain/keys.go
@@ -3,31 +3,47 @@ package GoBlockchain
 import "GoBlockchain/ed25519"
 
 const (
+
 	privateKeyLen = 64
 	publicKeyLen = 32
 
 )
 
 // Ed25519 is an elliptic curve cryptography (ECC) algorithm that provides digital signatures. 
-// It is based on the mathematical properties of a specific elliptic curve called the Ed25519 curve. 
-// The algorithm is designed for efficiency, security, and simplicity.
+// It is based on the mathematical properties of a specific elliptic curve called the Ed25519 curve, designed for efficiency, security, and simplicity.
 
 type PrivateKey struct {
 	key ed25519.PrivateKey
 }
 
+// Bytes returns the raw bytes of the private key.
 func (p * PrivateKey) Bytes() []byte {
 	return p.key
 }
 
+// Sign generates a digital signature for the given message using the private key.
 func (p *PrivateKey) Sign(msg []byte) []byte {
 	return ed25519.Sign(p.key, msg)
 }
 
-func (p *PrivateKey) Public() *PublicKey{
-	b := make([]byrw)
+/*
+In the Ed25519 elliptic curve algorithm, a private key is a 256-bit (32-byte) random number, 
+and the corresponding public key is derived from part of this private key. 
+The public key is essentially the second half (from byte 32 onwards) of the private key.
+*/
 
+func (p *PrivateKey) Public() *PublicKey{
+	// Create a byte slice 'b' with a length of publicKeyLen
+	b := make([]byte, publicKeyLen)
+	// Copy the second half of the private key 'p.key' to 'b'
+	copy(b, p.key[32:])
+	// Create and return a new PublicKey object with the bytes 'b'
+	return &PublicKey{
+		key: b,
+	}
 }
 
+// PublicKey represents the Ed25519 public key.
 func PublicKey struct {
 	key ed25519.PublicKey
+}
