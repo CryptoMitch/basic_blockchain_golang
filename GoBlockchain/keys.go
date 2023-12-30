28b74d6036ddedcd2ef248d24158e1e43016bc97
Mitch Hamilton <thecryptomitch@gmail.com>
2023-12-30 09:06:36 +0800

    initialize project as Go Module. Add dependencies

--- GoBlockchain/keys.go
+++ GoBlockchain/keys.go
@@ -1,5 +1,17 @@
 package GoBlockchain
 
+import "GoBlockchain/ed25519"
+
+const (
+	privateKeyLen = 64
+	publicKeyLen = 32
+
+)
+
+// Ed25519 is an elliptic curve cryptography (ECC) algorithm that provides digital signatures. 
+// It is based on the mathematical properties of a specific elliptic curve called the Ed25519 curve. 
+// The algorithm is designed for efficiency, security, and simplicity.
+
 type PrivateKey struct {
 	key ed25519.PrivateKey
 }
@@ -11,3 +23,11 @@ func (p * PrivateKey) Bytes() []byte {
 func (p *PrivateKey) Sign(msg []byte) []byte {
 	return ed25519.Sign(p.key, msg)
 }
+
+func (p *PrivateKey) Public() *PublicKey{
+	b := make([]byrw)
+
+}
+
+func PublicKey struct {
+	key ed25519.PublicKey
